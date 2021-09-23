de19309c2d961a8cac797694bb5aa165bf06c8eb
Augusto Souza <augustojsouza1@gmail.com>
2021-09-23 11:35:38 -0300

    Creating Export button logic

--- popup/popup.js
+++ popup/popup.js
@@ -1,16 +1,24 @@
 const launcher = document.querySelector('.launch-btn');
+const exportBtn = document.querySelector('.export-btn');
 let isRecording = "false";
 
 // verifica no chrome storage se está ou não gravando e então muda o texto do botão para "Record" ou "Stop" 
 chrome.storage.sync.get("recording", ({ recording }) => {
     isRecording = recording;
 
+    chrome.storage.sync.get("recording", ({ recording }) => {
+        isRecording = recording;
+    });
+
     if (isRecording === "true") {
         launcher.innerText = "Stop";
         launcher.classList.remove("launch-btn");
+        exportBtn.style.display = "none";
         launcher.classList.add("stop-btn");
+
     } else {
         launcher.innerHTML = "Record";
+        exportBtn.style.display = "inline";
         launcher.classList.remove("stop-btn");
         launcher.classList.add("launch-btn");
     }
@@ -23,6 +31,7 @@ launcher.addEventListener('click', (event) => {
     if (isRecording === "true") {  // quando estiver gravando(true) vai cair aqui ao clicar, parando de gravar
         launcher.innerText = "Record";
         launcher.classList.remove("stop-btn");
+        exportBtn.style.display = "inline";
         launcher.classList.add("launch-btn");
         isRecording = "false";
         chrome.storage.sync.set({ recording: "false" });
@@ -34,7 +43,7 @@ launcher.addEventListener('click', (event) => {
         console.log(isRecording);
 
         chrome.storage.sync.get("eventsTeste", (data) => {
-            postUserEvents(data);
+            // postUserEvents(data);
             console.log(data);
         });
 
@@ -42,6 +51,7 @@ launcher.addEventListener('click', (event) => {
         chrome.storage.sync.set({ eventsTeste: [] });
         launcher.innerText = "Stop";
         launcher.classList.remove("launch-btn");
+        exportBtn.style.display = "none";
         launcher.classList.add("stop-btn");
         isRecording = "true";
         chrome.storage.sync.set({ recording: "true" });
@@ -71,4 +81,4 @@ async function postUserEvents(data) {
     console.log(options);
   
     await fetch(url, options);
-  }
+}
