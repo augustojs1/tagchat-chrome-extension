413d3d4f311dd3db649327d14aa1b8d14d48257e
Augusto Souza <augustojsouza1@gmail.com>
2021-09-27 16:31:41 -0300

    Filter useless clicks

--- content/UserEvent.js
+++ content/UserEvent.js
@@ -1,45 +1,66 @@
 function handleUserEvent(event) {
-  const target = event.target;
-  const type = event.type;
-  if (type === "click") {
-    chrome.storage.sync.get(["eventsTeste"], (result) => {
-      result.eventsTeste.push({
-        URL: target.baseURI,
-        target: {
-          tag: target.localName,
-          class: target.className,
-          id: target.id,
-          name: target.name,
-          href: target.href ? target.href : "",
-          value: target.value ? target.value : "",
-        },
-        type,
-      });
+  const { target } = event;
+  const { type } = event;
 
-      console.log(result);
-      chrome.storage.sync.set({ eventsTeste: result.eventsTeste });
-    });
+  const whitelistTags = [
+    "a",
+    "button",
+    "img",
+    "svg",
+    "span",
+    "input",
+    "summary",
+    "label",
+  ];
+  const isWhitelisted = whitelistTags.some(isUsefullTag);
+  console.log(target.localName);
+
+  function isUsefullTag(currentValue) {
+    return currentValue === target.localName;
   }
 
-  if (type === "change") {
-    chrome.storage.sync.get(["eventsTeste"], (result) => {
-      result.eventsTeste.push({
-        URL: target.baseURI,
-        target: {
-          tag: target.localName,
-          class: target.className,
-          id: target.id,
-          name: target.name,
-          value: target.value,
-        },
-        type,
-      });
+  console.log(isWhitelisted);
 
-      console.log(result);
-      console.log("------------------------------------------------------");
-      console.log(event);
-      console.log("------------------------------------------------------");
-      chrome.storage.sync.set({ eventsTeste: result.eventsTeste });
-    });
+  if (isWhitelisted) {
+    if (type === "click") {
+      chrome.storage.sync.get(["eventsTeste"], (result) => {
+        result.eventsTeste.push({
+          URL: target.baseURI,
+          target: {
+            tag: target.localName,
+            class: target.className,
+            id: target.id,
+            name: target.name,
+            href: target.href ? target.href : "",
+            value: target.value ? target.value : "",
+          },
+          type,
+        });
+
+        console.log(result);
+        chrome.storage.sync.set({ eventsTeste: result.eventsTeste });
+      });
+    }
+
+    if (type === "change") {
+      chrome.storage.sync.get(["eventsTeste"], (result) => {
+        result.eventsTeste.push({
+          URL: target.baseURI,
+          target: {
+            tag: target.localName,
+            class: target.className,
+            id: target.id,
+            name: target.name,
+            value: target.value,
+          },
+          type,
+        });
+
+        console.log(result);
+        console.log("------------------------------------------------------");
+        console.log("------------------------------------------------------");
+        chrome.storage.sync.set({ eventsTeste: result.eventsTeste });
+      });
+    }
   }
 }
